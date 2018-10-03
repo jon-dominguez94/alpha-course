77d673a069f6702f98e8ab08b8ed43f4a60ebe1c
jon-dominguez94 <43150313+jon-dominguez94@users.noreply.github.com>
2018-10-03 09:57:25 -0700

    Add files via upload

--- jon_dominguez_rpn/lib/00_rpn_calculator.rb
+++ jon_dominguez_rpn/lib/00_rpn_calculator.rb
@@ -1,88 +1,86 @@
 class RPNCalculator
-  # TODO: your code goes here!
 
   def initialize
     @stack = []
-  end
-
-  def push(num)
-    @stack.push(num)
   end
 
   def value
     @stack.last
   end
 
+  def push(num)
+    @stack.push(num)
+  end
+
+  def empty?
+    @stack.length < 2
+  end
+
   def plus
-    if @stack.length < 2
-      raise "calculator is empty"
-    else
-      op2 = @stack.pop
-      op1 = @stack.pop
-      @stack.push(op1 + op2)
-    end
+    raise "calculator is empty" if empty?
+    op2 = @stack.pop
+    op1 = @stack.pop
+    @stack.push(op1 + op2)
   end
 
   def minus
-    if @stack.length < 2
-      raise "calculator is empty"
-    else
-      op2 = @stack.pop
-      op1 = @stack.pop
-      @stack.push(op1 - op2)
-    end
-  end
-
-  def divide
-    if @stack.length < 2
-      raise "calculator is empty"
-    else
-      op2 = @stack.pop
-      op1 = @stack.pop
-      @stack.push(op1.to_f / op2.to_f)
-    end
+    raise "calculator is empty" if empty?
+    op2 = @stack.pop
+    op1 = @stack.pop
+    @stack.push(op1 - op2)
   end
 
   def times
-    if @stack.length < 2
-      raise "calculator is empty"
-    else
-      op2 = @stack.pop
-      op1 = @stack.pop
-      @stack.push(op1.to_f * op2.to_f)
-    end
+    raise "calculator is empty" if empty?
+    op2 = @stack.pop
+    op1 = @stack.pop
+    @stack.push(op1 * op2)
+  end
+
+  def divide
+    raise "calculator is empty" if empty?
+    op2 = @stack.pop
+    op1 = @stack.pop
+    @stack.push(op1.to_f / op2)
   end
 
   def tokens(string)
-    string.split.map! do |el|
-      if el =~ /\d/
-        el.to_i
+    tokens = []
+    operators = "+-*/"
+    string.split.each do |ch|
+      if operators.include?(ch)
+        tokens << ch.to_sym
       else
-        el.to_sym
+        tokens << ch.to_i
       end
     end
+    tokens
   end
 
   def evaluate(string)
-    ops = [:+, :-, :*, :/]
-    toks = tokens(string)
-    toks.each do |t|
-      if ops.include?(t)
-        case t
-        when :+
-          self.plus
-        when :-
-          self.minus
-        when :*
-          self.times
-        when :/
-          self.divide
-        end
-      elsif t.is_a? Integer
-        self.push(t)
+    tokens = tokens(string)
+    tokens.each do |tok|
+      if tok.is_a? Fixnum
+        self.push(tok)
+      else
+        perform(tok)
       end
     end
-    @stack.pop
+    value
   end
 
+  private
+
+  def perform(token)
+    case token
+    when :+
+      plus
+    when :-
+      minus
+    when :*
+      times
+    when :/
+      divide
+    end
+  end
 end
